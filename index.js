7c94b629ce3b1542ee3b4adde2b3e969ce84e390
Dimitar <dimitar.mandarliev@gmail.com>
2024-09-15 19:51:00 +0300

    Listening to changes

--- index.js
+++ index.js
@@ -2,18 +2,27 @@
 
 // This function will return us the Store
 function createStore() {
-    // The Store will have 4 parts:
+  // The Store will have 4 parts:
 
-    // 1. The State
-    // 2. Getting the State
-    // 3. Listen to changes in the State
-    // 4. Updating the State
+  // 1. The State
+  // 2. Getting the State
+  // 3. Listen to changes in the State
+  // 4. Updating the State
 
-    let state;
+  let state;
+  let listeners = [];
 
-    const getState = () => state
+  const getState = () => state;
 
-    return{
-        getState
-    }
+  const listenToState = (listener) => {
+    listeners.push(listener);
+    return () => {
+      listeners = listeners.filter((l) => l !== listener);
+    };
+  };
+
+  return {
+    getState,
+    listenToState,
+  };
 }
