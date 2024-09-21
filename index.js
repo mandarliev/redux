b9765b901c25591e2061e91c934236387d7439d6
Dimitar <dimitar.mandarliev@gmail.com>
2024-09-21 17:00:34 +0300

    Combining todos & goals reducers

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
 // Reducer function
-function todos (state = [], action) {
-
-  switch(action.type) {
-    case 'ADD_TODO' :
-      return state.concat([action.todo])
-    case 'REMOVE_TODO' :
-      return state.filter((todo) => todo.id !== action.id)
-    case 'TOGGLE_TODO' :
-    return state.map((todo) => todo.id !== action.id ? todo : 
-    // ES6 feature
-      Object.assign({}, todo, {complete: !todo.complete})
-    )
-    default :
+function todos(state = [], action) {
+  switch (action.type) {
+    case "ADD_TODO":
+      return state.concat([action.todo]);
+    case "REMOVE_TODO":
+      return state.filter((todo) => todo.id !== action.id);
+    case "TOGGLE_TODO":
+      return state.map((todo) =>
+        todo.id !== action.id
+          ? todo
+          : // ES6 feature
+            Object.assign({}, todo, { complete: !todo.complete })
+      );
+    default:
       return state;
   }
+}
 
+function goals(state = [], action) {
+  switch (action.type) {
+    case "ADD_GOAL":
+      return state.concat([action.goal]);
+    case "REMOVE GOAL":
+      return state.filter((goal) => goal.id !== action.id);
+    default:
+      return state;
+  }
+}
+
+// I made this because I need to split the reducer into todos and goals
+function app(state = {}, action) {
+  return {
+    todos: todos(state.todos, action),
+    goals: todos(state.goals, action),
+  };
 }
 
 // This function will return us the Store
 function createStore(reducer) {
-
   // The Store will have 4 parts:
 
   // 1. The State
@@ -27,7 +45,7 @@ function createStore(reducer) {
   // 3. Listen to changes in the State
   // 4. Updating the State
 
-  let state
+  let state;
   let listeners = [];
 
   const getState = () => state;
@@ -43,14 +61,14 @@ function createStore(reducer) {
   // Like this store.dispatch(object)
   const dispatch = (action) => {
     // call todos in order to update the state
-    state = reducer(state, action)
+    state = reducer(state, action);
     // after updating the state we need to loop over the listensers and invoke each one of them so that they know the state was updated
-    listeners.forEach((listener) => listener())
-  }
+    listeners.forEach((listener) => listener());
+  };
 
   return {
     getState,
     listenToState,
-    dispatch
+    dispatch,
   };
 }
